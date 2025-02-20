8a2194c18aae2cd5b619494639c6c96364267bea
Abdarrahmane-NEINE <kaberneine10@gmail.com>
2025-02-20 20:27:22 +0100

    add simple protocol translation

---
+++ app/services/protocol_translation.py
@@ -0,0 +1,7 @@
+def translate_request(request_data: dict) -> dict:
+    # Convert client requestion response desired format for the service.
+    return request_data
+
+def translate_response(response_data: dict) -> dict:
+    # Convert backend response to desired format for the client.
+    return response_data
